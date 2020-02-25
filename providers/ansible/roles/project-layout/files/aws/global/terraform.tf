330710141b60b4ecbdff746f83b23a5d5c0601bd
Andrey Odessky2 Rogovsky <andrey.rogovsky@itomy.ch>
2020-02-25 08:36:15 +0200

    bash

--- providers/ansible/roles/project-layout/files/aws/global/terraform.tf
+++
@@ -1,5 +0,0 @@
-terraform {
-  required_version = "0.12.18"
-  # Intentionally empty. Will be filled by Terragrunt.
-  backend "s3" {}
-}
--- providers/ansible/roles/project-layout/files/aws/global/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {}
-variable "terraform_remote_state_s3_bucket" {}
-variable "terraform_remote_state_dynamodb_table" {}
-variable "terraform_remote_state_file_name" {}
-variable "terraform_remote_state_kms_key" {}
